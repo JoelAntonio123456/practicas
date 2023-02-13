d73ae91655068ff8327a766340adf76f632c41f5
JoelAntonio123456 <yoelyoelhuamani@gmail.com>
2023-02-13 02:26:47 -0500

    se agrega el proyecto

--- 3.js
+++ 3.js
@@ -1,12 +1,10 @@
-//variables
+
 const inputRandomNumber = document.getElementById('inputRandom');
 
-//eventos
 window.addEventListener('load', () => {
     getRandomNumber();
 })
 
-//funciones
 function getRandomNumber() {
     let randomNumber = Math.random().toString().slice(2, 8);
     inputRandomNumber.value = randomNumber;
