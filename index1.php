1d26ce85f6ef2e1e65f23b6220b677dec15bd860
JoelAntonio123456 <yoelyoelhuamani@gmail.com>
2023-02-12 21:57:04 -0500

    first commint

--- index1.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION["nombre_1"] = "jaimito";
-$_SESSION["nombre_2"] = "pedrito";
-$_SESSION["session_id"] = session_id();
-?>
-<a href="sesiones.php">envio de variable de sesion</a>
-
--- sesiones.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-echo $_SESSION["nombre_1"];
-echo '<br>';
-print_r($_SESSION); 
-?>
